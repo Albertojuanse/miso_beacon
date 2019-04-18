c4cfa848b7d29b8c09dc5bfbcd908b27391f6904
Sebastián Lombraña, A <aj.sebastian@alumnos.upm.es>
2019-04-18 18:35:31 +0200

    Point class

--- miso_beacon_radiodet/Position.py
+++ miso_beacon_radiodet/Position.py
@@ -1,22 +1,171 @@
 """This class defines a single point in the space"""
 
+from math import sqrt, pow, pi, sin, cos, acos, atan
+
 
 class Position:
 
-    def __init__(self, x=0, y=0, z=0):
+    def __init__(self, x=0.0, y=0.0, z=0.0):
         """Constructor"""
         self.x = x
         self.y = y
         self.z = z
 
-    @property
-    def x(self):
+    # Cartesian getters and setters
+    def setx(self, x=0):
+        """X coordinate setter"""
+        self.x = x
+
+    def getx(self):
+        """X coordinate getter"""
         return self.x
 
-    @property
-    def y(self):
+    def sety(self, y=0):
+        """Y coordinate setter"""
+        self.y = y
+
+    def gety(self):
+        """Y coordinate getter"""
         return self.y
 
-    @property
-    def z(self):
+    def setz(self, z=0):
+        """Z coordinate setter"""
+        self.z = z
+
+    def getz(self):
+        """Z coordinate getter"""
         return self.z
+
+    # Polar getters and setters
+    def setrho(self, rho=0):
+        """Rho coordinate setter"""
+        # Get current phi value
+        phi = 0.0
+        if self.x > 0 and self.y >= 0:
+            phi = atan(self.y / self.x)
+        elif self.x == 0 and self.y > 0:
+            phi = pi/2
+        elif self.x < 0:
+            phi = atan(self.y / self.x) + pi
+        elif self.x == 0 and self.y < 0:
+            phi = 3*pi/2
+        elif self.x > 0 and self.y < 0:
+            phi = atan(self.y / self.x) + 2*pi
+
+        # Set new rho value
+        self.x = rho * cos(phi)
+        self.y = rho * sin(phi)
+
+    def getrho(self):
+        """Rho coordinate getter"""
+        return sqrt(pow(self.x, 2) + pow(self.y, 2))
+
+    def setphi(self, phi=0):
+        """Phi coordinate setter"""
+        # Get current rho value
+        rho = sqrt(pow(self.x, 2) + pow(self.y, 2))
+
+        # Set new phi value
+        self.x = rho * cos(phi)
+        self.y = rho * sin(phi)
+
+    def getphi(self):
+        """Phi coordinate getter"""
+        phi = 0.0
+        if self.x > 0 and self.y >= 0:
+            phi = atan(self.y / self.x)
+        elif self.x == 0 and self.y > 0:
+            phi = pi/2
+        elif self.x < 0:
+            phi = atan(self.y / self.x) + pi
+        elif self.x == 0 and self.y < 0:
+            phi = 3*pi/2
+        elif self.x > 0 and self.y < 0:
+            phi = atan(self.y / self.x) + 2*pi
+        return phi
+
+    # Spherical getters and setters
+    def setrad(self, rad=0):
+        """Radial coordinate setter"""
+        # Get current inclination and azimuth value
+        if self.x == 0 and self.y == 0 and self.z == 0:
+            inc = acos(self.z / sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2)))
+        else:
+            inc = 0.0
+        if self.z == 0:
+            azi = 0
+        else:
+            azi = atan(self.y / self.z)
+
+        # Set new radial value
+        self.x = rad * sin(inc) * cos(azi)
+        self.y = rad * sin(inc) * sin(azi)
+        self.z = rad * cos(inc)
+
+    def getrad(self):
+        """Radial coordinate getter"""
+        return sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+
+    def setinc(self, inc=0):
+        """Inclination coordinate setter"""
+        # Get current azimuth and radial value
+        rad = sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+        if self.z == 0:
+            azi = 0
+        else:
+            azi = atan(self.y / self.z)
+
+        # Set new inclination value
+        self.x = rad * sin(inc) * cos(azi)
+        self.y = rad * sin(inc) * sin(azi)
+        self.z = rad * cos(inc)
+
+    def getinc(self):
+        """Inclination coordinate getter"""
+        if self.x == 0 and self.y == 0 and self.z == 0:
+            return 0.0
+        else:
+            return acos(self.z / sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2)))
+
+    def setazi(self, azi=0):
+        """Azimuth coordinate setter"""
+        # Get current inclination and radial value
+        rad = sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2))
+        if self.x == 0 and self.y == 0 and self.z == 0:
+            inc = acos(self.z / sqrt(pow(self.x, 2) + pow(self.y, 2) + pow(self.z, 2)))
+        else:
+            inc = 0.0
+
+        # Set new azimuth value
+        self.x = rad * sin(inc) * cos(azi)
+        self.y = rad * sin(inc) * sin(azi)
+        self.z = rad * cos(inc)
+
+    def getazi(self):
+        """Azimuth coordinate getter"""
+        if self.z == 0:
+            return 0.0
+        else:
+            return atan(self.y / self.z)
+
+    # Location setters
+    def setcartesianlocation(self, x=0, y=0, z=0):
+        """Cartesian location setter; (x, y, z) is required"""
+        self.setx(x)
+        self.sety(y)
+        self.setz(z)
+
+    def setcilindricallocation(self, rho=0, phi=0, z=0):
+        """Cilindrical location setter; (rho, phi, z) is required"""
+        self.setrho(rho)
+        self.setphi(phi)
+        self.setz(z)
+
+    def setsphericallocation(self, rad=0, inc=0, azi=0):
+        """Cilindrical location setter; (rho, phi, z) is required"""
+        self.setrad(rad)
+        self.setinc(inc)
+        self.setazi(azi)
+
+    def __str__(self):
+        return "(" + str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ")"
